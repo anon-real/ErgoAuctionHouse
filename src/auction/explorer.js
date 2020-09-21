629e3cb377131475945cd7c78c4496917a652efa
anon_real <anon_real_anon@protonmail.com>
2020-09-21 23:56:32 +0330

    Improved active auction ui

--- src/auction/explorer.js
+++ src/auction/explorer.js
@@ -2,6 +2,7 @@ import {Address, Explorer, Transaction, ErgoBox} from "@coinbarn/ergo-ts";
 import axios, { AxiosInstance, AxiosPromise, AxiosResponse } from 'axios';
 
 const explorer = Explorer.mainnet;
+const auctionAddress = "PLHeEg8w7y5tNkJkcKhK9bd4qvubZ4xFMDjrHPTvFxoj4WA86rp6kHzj5XgrpWQJgxnaQ3N6phdoHwoqxGUpT4fnee5BtbTcic6K7FWaNFAWEBc4co1KcanGBVJNKT42qxdygppgN2jpcSfBY3CSVQ78YrfPmaqZUBs1M8Yahb2XLbEPAsRYwJsfwTQUB2qumzv5kxEppHVmdtqqKwcryQSvyqEsvgLDPpG7YdiXVmhDeLdoacMKEtw3gEkV4Y4RcEhhS4SWooyrtfZibqgfYAfPfEjVmCiZwtwnzTVPFLjWnmHdPabWiQ9ku63WxzSrCKqKVF6npPV"
 
 async function getRequest(url) {
     return explorer.apiClient({
@@ -19,15 +20,13 @@ export function getTokenInfo(token) {
 }
 
 export function getActiveAuctions() {
-    let address = '9gAKeRu1W4Dh6adWXnnYmfqjCTnxnSMtym2LPPMPErCkusCd6F3'
-    return explorer.getUnspentOutputs(new Address(address))
+    return explorer.getUnspentOutputs(new Address(auctionAddress))
         .then(boxes => boxes.filter(box => box.assets.length > 0))
 
 }
 
 export async function getTokenTx(tokenId) {
     const {data} = await getRequest(`/transactions/boxes/${tokenId}`);
-    console.log(data)
     return await data.spentTransactionId
 }
 
--- src/auction/helpers.js
+++ src/auction/helpers.js
@@ -3,7 +3,7 @@ import React from "react";
 const explorerUrl = "https://explorer.ergoplatform.com/en/"
 
 export function friendlyToken(token) {
-    return token.amount + ' of ' + token.tokenId.slice(0, 10) + '...' + token.tokenId.slice(-10) + ' token';
+    return token.amount + ' of ' + token.tokenId.slice(0, 13) + '...' + token.tokenId.slice(-13) + ' token';
 }
 
 export function getTxUrl(txId) {
