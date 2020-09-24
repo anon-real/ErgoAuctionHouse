fa443d8d4c2e7f5113b0f993a492d964888024e1
anon_real <anon_real_anon@protonmail.com>
2020-09-24 15:47:04 +0330

    Add new auction and new bid modals

--- src/auction/nodeWallet.js
+++ src/auction/nodeWallet.js
@@ -1,18 +1,30 @@
-import {post, get} from './rest'
+import {post, get} from './rest';
 
 function getUrl(url) {
-    if (!url.startsWith('http')) url = 'http://' + url
-    if (url.endsWith('/')) url = url.slice(0, url.length - 2)
-    return url
+    if (!url.startsWith('http')) url = 'http://' + url;
+    if (url.endsWith('/')) url = url.slice(0, url.length - 2);
+    return url;
 }
 
 export async function getInfo(url) {
-    return get(getUrl(url) + '/info')
-        .then(res => res.json())
+    return get(getUrl(url) + '/info').then((res) => res.json());
 }
 
-export async function getAddress(url, apiKey) {
-    return await post(getUrl(url) + '/wallet/deriveKey', {derivationPath: 'm'}, apiKey)
-        .then(res => res.json())
+export async function getAddress(
+    url = JSON.parse(sessionStorage.getItem('wallet')).url,
+    apiKey = JSON.parse(sessionStorage.getItem('wallet')).apiKey
+) {
+    return await post(
+        getUrl(url) + '/wallet/deriveKey',
+        {derivationPath: 'm'},
+        apiKey
+    ).then((res) => res.json());
 }
 
+export async function getAssets(
+    url = JSON.parse(sessionStorage.getItem('wallet')).url,
+    apiKey = JSON.parse(sessionStorage.getItem('wallet')).apiKey
+) {
+    return await get(getUrl(url) + '/wallet/balances', apiKey)
+        .then((res) => res.json())
+}
