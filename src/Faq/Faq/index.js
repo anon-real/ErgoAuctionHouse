003b6a2e18632521514c0562d1efb6168a370dac
anon_real <anon_real_anon@protonmail.com>
2020-09-30 21:00:35 +0330

    Add some QA to FAQ page

--- src/Faq/Faq/index.js
+++ src/Faq/Faq/index.js
@@ -6,18 +6,46 @@ import Faq from 'react-faq-component';
 const data = {
     rows: [
         {
-            title: 'Lorem ipsum dolor sit amet,',
-            content: 'Lorem ipsum dolor sit amet, consectetur ',
+            title: 'What tokens can I auction?',
+            content: 'You can auction any token that you think is valuable somehow. Whether it is because of PoW done on creating it or it is representing digital art, etc.',
         },
         {
-            title: 'Nunc maximus, magna at ultricies elementum',
+            title: 'How can I trust Ergo Auction with my wallet info?',
             content:
-                'Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam.',
+                'Ergo Auction is open source and serverless. ' +
+                ' You wallet info will be saved in the session storage of your browser securely and will be removed as soon as you close the app.',
         },
         {
-            title: 'Curabitur laoreet, mauris vel blandit fringilla',
+            title: 'When is my wallet info used?',
             content:
-                'Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc',
+                'Your wallet info will be used only when you confirm starting and auction or placing a bid on an existing one.',
+        },
+        {
+            title: 'What happens to my ERG when I place a bid?',
+            content:
+                'Lets say you have placed a 10 ERG bid on an auction, then 10 ERG from your wallet will be locked with the auction contract.\
+                 If you win the auction, then you will receive the auctioned token and your 10 ERG will be sent to the seller. Otherwise, \
+                  immediately after someone places a bid bigger than yours, your 10 ERG will be returned to you.',
+        },
+        {
+            title: 'What happens if I start an auction but no one places bid on it?',
+            content:
+                'Basically when you start an auction, you are the first bidder for the auction. If auction duration finishes ' +
+                'and you remain the only bidder, then both your initial bid and your auctioned token will be returned to you.',
+        },
+        {
+            title: "I have started an auction but can't see it in the Active Auction page!",
+            content:
+                'Starting an auction is basically waiting for an transaction representing the auction to be mined. ' +
+                'Depending on the network traffic, it may take a few minutes for the transaction to be mined and you see your auction. ' +
+                'if you keep the app open, you will be notified about the status.',
+        },
+        {
+            title: "I have place a bid but it hasn't effected the auction yet!",
+            content:
+                'Just like when you start an auction, you have to wait for your bid transaction to be mined. ' +
+                'It is possible that your bid transaction gets rejected because others also have placed bid on the auction. ' +
+                'In any condition, if you keep the app open, you will be notified of the status.',
         },
     ],
 };
