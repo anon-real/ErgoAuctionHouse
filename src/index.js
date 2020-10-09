9d1ffacbbf337bb62f84d2c44a1cbd5a810f591a
anon_real <anon_real_anon@protonmail.com>
2020-10-09 21:24:06 +0330

    Add auction fee support

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import registerServiceWorker from './registerServiceWorker';
-import { unregister } from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './assets/base.css';
 import Main from './AuctionPages/Main';
 import configureStore from './config/configureStore';
-import { Provider } from 'react-redux';
-import { handlePendingBids } from './auction/explorer';
-import {unspentBoxes} from "./auction/nodeWallet";
+import {Provider} from 'react-redux';
+import {auctionNFT, dataInputAddress, additionalData, handlePendingBids, unspentBoxesFor} from './auction/explorer';
+import {showMsg} from "./auction/helpers";
 
 const store = configureStore();
 const rootElement = document.getElementById('root');
 
 const renderApp = (Component) => {
+    function updateDataInput() {
+        unspentBoxesFor(dataInputAddress)
+            .then((res) => {
+                return res.filter(
+                    (box) =>
+                        box.assets.length > 0 &&
+                        box.assets[0].tokenId === auctionNFT
+                );
+            })
+            .then((res) => additionalData['dataInput'] = res[0])
+            .catch(() => showMsg('Could not load data input from explorer...', false, true))
+    }
+
+    updateDataInput()
     setInterval(handlePendingBids, 60000);
+    setInterval(() => {
+        updateDataInput()
+    }, 120000)
+
     ReactDOM.render(
         <Provider store={store}>
             <HashRouter>
-                <Component />
+                <Component/>
             </HashRouter>
         </Provider>,
         rootElement
