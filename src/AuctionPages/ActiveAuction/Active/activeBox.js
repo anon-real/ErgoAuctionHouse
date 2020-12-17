04f8909d898e0dbd5f89ad1348c86654f8d481c3
anon_real <anon_real_anon@protonmail.com>
2020-12-17 20:41:30 +0330

    Fix some minor UI issues

--- src/AuctionPages/ActiveAuction/Active/activeBox.js
+++ src/AuctionPages/ActiveAuction/Active/activeBox.js
@@ -118,7 +118,7 @@ export default class ActiveBox extends React.Component {
                             </span>
                             {this.props.box.isArtwork && <span
                                 onClick={() => this.setState({artDetail: true})}
-                                data-tip="Artwork NFT"
+                                data-tip="Artwork NFT Detail"
                                 className="icon-wrapper rounded-circle opacity-7 m-2 font-icon-wrapper">
                                 <i className="lnr-picture icon-gradient bg-plum-plate fsize-4"/>
                                 <ArtworkDetails
@@ -212,7 +212,7 @@ export default class ActiveBox extends React.Component {
                                     justifyContent: 'center',
                                     alignItems: 'center',
                                     height: '60px',
-                                    overflowY: 'auto',
+                                    overflowY: 'hidden',
                                     overflowX: 'hidden'
                                 }}
                             >
