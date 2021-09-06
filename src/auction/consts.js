58c0f25271202e12dfab7778ce5b5b12a7c292a7
anon_real <anon_real_anon@protonmail.com>
2021-09-06 22:53:54 +0430

    Fix all issues regarding placing bids for custom currency

--- src/auction/consts.js
+++ src/auction/consts.js
@@ -1,6 +1,6 @@
 import {Address} from "@coinbarn/ergo-ts";
 
-export const auctionAddress = `XaNB7EUoXT5Q7QNUHpHsbnzC2Qf9ndZEqd9DnYMWGJmVWgitGpfvacuVMwakZCdpUbp8kYcpQg5fgTJPWPqPsjzdc3trKc87yTee1SxvoFN3fjXrLfesMe6ioSAxZ3Beh6uqALZwg8VEM8e12yBspAv1nCDhFrkkEY1qHcsqGiRLjbmjkJzAzSvRZeZArWBbcbgcFJjXkBDQ9t4F9PgVBkq8bBsKDWqkLkiSV4SEiv5JJEAkkn8ULquysuDj6sZ1LMVKVur2Pv7thyWpft9df11hVnJh3WU6HkadtmStTsQmFzLwtB5LGqgzbNsbugDfQ9NFWzeMU9YMEkjHbb7f7PbrRruaqEijN2oLuqqHrZDhTJwLpCE6gaNtHGv9J1VJrLr4BL8JL5E7Uti3a9N1RyDwNKysE34HDnN93Q7msPdYkbLDbg7ZDGpRCy62KFYGUc1SwwsNNqHfefb1ytSVj4o8ui9HP3goLPx4vCVXQsCCM7xVXy1qmXcv5pdGhbrHeh4BXPjpbdCoZBsgtJ3U8PbGRAzQAq3391jgqLyKukaoGeMnH3swShYWpbRcsLQ6K39WdVEQ4KJtWS5DPyJ3VzyJsnwy85wuQ1azcRjykHPnhqzLwYzdMVNzTxpptjexAn3xdgU2u2F6o4WNwqFEZ2H9z2o3WnhFYYTPVhMvAvxjGo4iqSTcszKJ455jNMmbgeb7wAhxcV5rUBdGPuSex6vhf8AWfmrUH7QnYNSPthkqicZiFv9qmwdTQPZxHwx6PHwMBfdVBdPwGgrsQNdnvp5x5LjeDYGwPiDvjPw911gt7`
+export const auctionAddress = `3cVSf8PPdT3GVE782iaguCye3dFXxbU5z9ah5AfxuQ6SjLrnYYj9H1R6CjqQNF6vzwMqTcYLJTjeK3TWSENwaEw3Uu36nuZHRK7Py9Rydt7pSFKQ4uSn7RvpoMAidjx9kar1GDf54PNTB8tmVvCHCwuNG4YUnNHjkF3PhK8dwo6wybxt5xYzxDosjb1ddZzLLZryMGBzJutHdjyBDK8JinbkjGQ8fDwKxDL5T6nW5XTsoU2NWLngfky8AzF1y4nn6mPShN7pTNdjSGCVhAZBsGt3i5tVVnuu1kD4kffvSpEcWBrVuMijbxzjhQCrsphozEbVj13Loj3RQuewjzAfHcqpbp17KWjAZtMugi9xvAct9BhFBBS6sEDjBLdWtcFbkp5NbJcZECe3UxBUMKzN9nrdZ84GVKoeGC4dFbf7rM1HTGBv2HNoK21ymzuGDkQokGkcCzHWSkt8u9mcZHVk3NCthzDckY2Xjg9qoJWPt5ngF2f7g3KkMt14aj2PYrArnWMPKz8Sf94owhgdg2BVHDtQhwqwTavW2hTdbzC84Vjjf3j8q9nmJEc8F3PUHi9PJBFqf5y8nCpT4NH4NZobCwpMKQtseL6AwyAMgEs1tSyXKKaJnQwarCe7Mru9LErpT65SdYsXRH8LaHM9a8kKm1D111kC8SrMFfVGbGL8LonzznWq6Cd7erxUFkGQeZceabwet6secgZRw6nCb7PKxqXuMqMEwxbN68Jd1ShqFPHzW25JSZGwhQdDMsVs1qTqxryL7AGnPVTpTbMpFxYKLYpyL2TTb5eNYivVM21SgjqrbLF1panFsDSvBCWfAEoBsVuxjTSLBJaJXcAv1HmJuU7EpmKry468sdWHmWXmM1AZ74ngXgTW4U3hr7gSGneimxYozziL26QZogZQcFeKuPNirXoH39jhUJVfZcdJ2EQ73WXtcr4sUT4QpZF1d`
 export const auctionAddresses = [auctionAddress]
 export let additionalData = {};
 export const auctionTrees = [auctionAddress] // array of trees of all auction addresses until now.
@@ -31,5 +31,13 @@ export const supportedCurrencies = {
         id: '03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04',
         decimal: 2,
         minSupported: 10
+    },
+    TestUSD: {
+        name: 'TestUSD',
+        id: '77597c7594ab183f2ae4b5cd388291505c14dca3e1308e40952239a65a053694',
+        decimal: 2,
+        minSupported: 10
     }
 }
+// export const assmUrl = 'https://assembler.ergoauctions.org/';
+export const assmUrl = 'localhost:9000/';
--- src/auction/newBidAssm.js
+++ src/auction/newBidAssm.js
@@ -2,75 +2,109 @@ import {addAssemblerBid, getWalletAddress,} from './helpers';
 import {Address} from '@coinbarn/ergo-ts';
 import {encodeHex, encodeNum} from './serializer';
 import {follow, p2s} from "./assembler";
-import {additionalData, txFee} from "./consts";
+import {additionalData, auctionAddress, contracts, supportedCurrencies, txFee} from "./consts";
 
 const template = `{
   val userAddress = PK("$userAddress")
   val bidAmount = $bidAmountL
-  val endTime = $endTime
+  val currencyId = fromBase64("$currencyId")
   val placeBid = {
-    HEIGHT < endTime && INPUTS(INPUTS.size - 1).id == fromBase64("$auctionId") &&
-      OUTPUTS(0).R8[Coll[Byte]].get == userAddress.propBytes && OUTPUTS(0).value == bidAmount
+    INPUTS(INPUTS.size - 1).id == fromBase64("$auctionId") &&
+      OUTPUTS(0).R5[Coll[Byte]].get == userAddress.propBytes && 
+      ((currencyId.size == 0 && OUTPUTS(0).value == bidAmount) ||
+         (OUTPUTS(0).tokens(1)._1 == currencyId && OUTPUTS(0).tokens(1)._2 == bidAmount))
   }
   val returnFunds = {
-    val total = INPUTS.fold(0L, {(x:Long, b:Box) => x + b.value}) - 4000000
-    OUTPUTS(0).value >= total && OUTPUTS(0).propositionBytes == userAddress.propBytes
+    val total = INPUTS.fold(0L, {(x:Long, b:Box) => x + b.value}) - 2000000
+    OUTPUTS(0).value >= total && OUTPUTS(0).propositionBytes == userAddress.propBytes && OUTPUTS.size == 2
   }
   sigmaProp(placeBid || returnFunds)
 }`;
 
-export async function registerBid(currentHeight, bidAmount, box, address) {
+export async function registerBid(block, bidAmount, box) {
     let ourAddr = getWalletAddress();
-    let tree = new Address(ourAddr).ergoTree;
-    let encodedTree = await encodeHex(tree);
+    let userTree = new Address(ourAddr).ergoTree;
+    const p2s = (await getBidP2s(bidAmount, box)).address
 
-    let nextEndTime =
-        box.finalBlock - currentHeight <= 'extendThreshold' &&
-        box.ergoTree === 'auctionWithExtensionTree' // TODO fix
-            ? box.finalBlock + 'extendNum'
-            : box.finalBlock;
-    if (nextEndTime !== box.finalBlock)
-        console.log(
-            `extended from ${box.finalBlock} to ${nextEndTime}. height: ${currentHeight}`
-        );
-    let encodedNextEndTime = await encodeNum(nextEndTime, true);
+    let nextEndTime = box.endTime
+    if (box.endTime - block.timestamp <= contracts[auctionAddress].extendThreshold) {
+        box.endTime += contracts[auctionAddress].extendNum
+    }
 
-    let newBox = {
-        value: bidAmount,
-        address: Address.fromErgoTree(box.ergoTree).address,
-        assets: box.assets,
-        registers: {
-            R4: box.additionalRegisters.R4,
-            R5: encodedNextEndTime,
-            R6: box.additionalRegisters.R6,
-            R7: box.additionalRegisters.R7,
-            R8: encodedTree,
-            R9: box.additionalRegisters.R9,
-        },
-    };
+    let auctionErg = bidAmount
+    let start = {
+        erg: bidAmount + txFee,
+    }
+    let auctionAssets = [
+        {
+            tokenId: box.assets[0].tokenId,
+            amount: box.assets[0].amount
+        }
+    ]
     let returnBidder = {
         value: box.value,
         address: box.bidder,
     };
-    let request = {
-        address: address,
-        returnTo: ourAddr,
-        startWhen: {
-            erg: bidAmount + txFee,
+    if (box.assets.length > 1) {
+        start = {}
+        start[box.assets[1].tokenId] = bidAmount
+
+        auctionErg = box.value
+        auctionAssets = [
+            {
+                tokenId: box.assets[0].tokenId,
+                amount: box.assets[0].amount
+            },
+            {
+                tokenId: box.assets[1].tokenId,
+                amount: bidAmount,
+            },
+        ]
+
+        returnBidder = {
+            value: -1,
+            address: box.bidder,
+            assets: [
+                {
+                    tokenId: box.assets[1].tokenId,
+                    amount: box.assets[1].amount
+                }
+            ]
+        }
+    }
+
+    let newBox = {
+        value: auctionErg,
+        address: auctionAddress,
+        assets: auctionAssets,
+        registers: {
+            R4: box.additionalRegisters.R4,
+            R5: await encodeHex(userTree),
+            R6: box.additionalRegisters.R6,
+            R7: await encodeNum(nextEndTime.toString()),
+            R8: box.additionalRegisters.R8,
+            R9: box.additionalRegisters.R9,
         },
+    };
+    let request = {
+        // address: "4MQyML64GnzMxZgm",
+        address: p2s,
+        returnTo: ourAddr,
+        startWhen: start,
         txSpec: {
             requests: [newBox, returnBidder],
             fee: txFee,
             inputs: ['$userIns', box.id],
-            dataInputs: [additionalData.dataInput.id],
+            dataInputs: [additionalData.dataInput.boxId],
         },
     };
+    console.log(request)
     return await follow(request)
         .then((res) => {
             if (res.id !== undefined) {
                 let bid = {
                     id: res.id,
-                    msg: "Your bid is being placed by the assembler service, see 'My Bids' section for more details.",
+                    msg: "Your bid is being placed, see 'My Bids' section for more details.",
                     info: {
                         token: box.assets[0],
                         boxId: box.id,
@@ -82,23 +116,31 @@ export async function registerBid(currentHeight, bidAmount, box, address) {
                             nextEndTime === box.finalBlock,
                         status: 'pending mining',
                         amount: bidAmount,
+                        currency: supportedCurrencies[box.currency],
                         isFirst: false,
                     },
                 };
                 addAssemblerBid(bid);
             }
+            res.address = p2s
             return res;
         });
 }
 
 export async function getBidP2s(bid, box) {
     let id64 = Buffer.from(box.id, 'hex').toString('base64');
+    let currencyId = ''
+    if (box.assets.length > 1) currencyId = box.assets[1].tokenId
+    currencyId = Buffer.from(currencyId, 'hex').toString('base64');
+
     let script = template
         .replace('$userAddress', getWalletAddress())
         .replace('$bidAmount', bid)
-        .replace('$endTime', box.finalBlock)
         .replace('$auctionId', id64)
+        .replace('$currencyId', currencyId)
         .replaceAll('\n', '\\n');
+    // console.log(script)
+    // return
     return p2s(script);
 }
 
