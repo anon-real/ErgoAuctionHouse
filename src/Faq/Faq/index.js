065822ec212532ce0022601e1932c0d8b45f4e3d
anon_real <anon_real_anon@protonmail.com>
2021-10-05 15:17:52 +0330

    Add indexedDb

--- src/Faq/Faq/index.js
+++ src/Faq/Faq/index.js
@@ -2,22 +2,36 @@ import React, {Fragment} from 'react';
 
 import PageTitle from '../../Layout/AppMain/PageTitle';
 import Faq from 'react-faq-component';
+import {additionalData} from "../../auction/consts";
 
 const data = {
     rows: [
         {
             title: 'What is the auction fee?',
-            content: 'Some small percentage of the final bid will be deducted as the auction fee. This percentage is currently set to 2%.',
+            content: `If an auction is finished and has a winner, 2%
+            of the final bid will go to the Ergo Auction House as compensation for its services and further development.`,
+        },
+        {
+            title: 'Start fee',
+            content: `To prevent spams and reduce scams, there is a starting fee of 0.1 ERG for auctions.`
+        },
+        {
+            title: 'How can I mint artworks',
+            content: 'Setup your wallet first, then go to Owned Artworks section and click on Create Artwork.',
+        },
+        {
+            title: 'What is minting artwork fee',
+            content: 'There is no fee for minting new artworks.',
         },
         {
             title: 'What tokens can I auction?',
             content: 'You can auction any token that you think is valuable somehow. Whether it is Artwork NFT, PoW-backed NFTs or it is representing some business shares, etc.',
         },
         {
-            title: 'How can I trust Ergo Auction House with my wallet info?',
+            title: 'How can I trust Ergo Auction House?',
             content:
-                'Ergo Auction House is open source and serverless.' +
-                ' The hosting website is only there to serve the app for your browser, all the other things will be done locally without any communication with any servers.'
+                'Ergo Auction House is community-driven and open source.' +
+                'The code, smart contracts and proxy contract are all audited and verifiable by all.'
         },
         {
             title: 'When is my wallet info used?',
@@ -25,30 +39,16 @@ const data = {
                 'Your wallet info will be used only when you confirm starting an auction or placing a bid on an existing one.',
         },
         {
-            title: 'What happens to my ERG when I place a bid?',
+            title: 'What happens to my funds when I place a bid?',
             content:
-                'Lets say you have placed a 10 ERG bid on an auction, then 10 ERG from your wallet will be locked within the auction contract.\
-                 If you win the auction, then you will receive the auctioned token and your 10 ERG will be sent to the seller. Otherwise, \
-                  immediately after someone places a higher bid, your 10 ERG will be returned to you.',
+                'Your ERGs or any other currencies, will be locked in the auction smart contract. If you win the auction,' +
+                'they will be sent to the seller in the same transaction that will send you your NFT/token. If you get outbidded,' +
+                ' your funds will be immediately returned to your wallet.',
         },
         {
             title: 'What happens if I start an auction but no one places bid on it?',
             content:
-                'You\'ll receive your token back while not being charged any fees.',
-        },
-        {
-            title: "I have started an auction but can't see it in the Active Auction page!",
-            content:
-                'Starting an auction is basically waiting for an transaction representing the auction to be mined. ' +
-                'Depending on the network traffic, it may take a few minutes for the transaction to be mined and you see your auction. ' +
-                'if you keep the app open, you will be notified about the status.',
-        },
-        {
-            title: "I have placed a bid but it hasn't effected the auction yet!",
-            content:
-                'Just like when you start an auction, you have to wait for your bid transaction to be mined. ' +
-                'It is possible that your bid transaction gets rejected because others also have placed bid on the auction. ' +
-                'In any condition, if you keep the app open, you will be notified of the status.',
+                'You\'ll receive your token back while not being charged with any fees (other than the starting fee).',
         },
     ],
 };
