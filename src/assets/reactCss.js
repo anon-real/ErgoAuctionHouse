ce2e61777b3395fbb7d7a3012a9b7f8e3ecd9a57
anon_real <anon_real_anon@protonmail.com>
2021-12-24 12:26:06 -0800

    Improve description

---
+++ src/assets/reactCss.js
@@ -0,0 +1,10 @@
+export const textStyle = {
+    maxWidth: '100%',
+    height: '55px',
+    display: '-webkit-box',
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: 3,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+};
+
